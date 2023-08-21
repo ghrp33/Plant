d1a45ec4afeaf550c09976418645a7ec91cb58d3
sergio santana <sergiosantana.m@gmail.com>
2023-08-21 17:10:41 -0400

    atualiced schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_21_192505) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_21_203004) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,4 +22,27 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_192505) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "requests", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "plante_id", null: false
+    t.bigint "user_id", null: false
+    t.index ["plante_id"], name: "index_requests_on_plante_id"
+    t.index ["user_id"], name: "index_requests_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "requests", "plantes"
+  add_foreign_key "requests", "users"
 end
