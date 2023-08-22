a096f87dd29e2b8c63374c2cda3e4057498632bc
sergio santana <sergiosantana.m@gmail.com>
2023-08-22 12:29:45 -0400

    migrate AddNameToUser

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,2 +1,18 @@
 class RequestsController < ApplicationController
+  def index
+    @requests = Request.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+    @request = @plant.requests.find(params[:id])
+  end
+
+  def update
+  end
 end
