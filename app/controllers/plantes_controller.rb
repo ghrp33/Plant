eefdea55bad8e65770dcdef268eec5c7fa3298c7
Marcela Langarica Mejia <mlangaricamejia@gmail.com>
2023-08-22 14:34:54 -0400

    added new and create for plante

--- app/controllers/plantes_controller.rb
+++ app/controllers/plantes_controller.rb
@@ -7,8 +7,19 @@ class PlantesController < ApplicationController
   end
 
   def new
+    @plante = Plante.new
   end
 
   def create
+    @plante = Plante.new(plante_params)
+    @plante.save
+    redirect_to plante_path(@plante)
   end
+
+  private
+
+  def plante_params
+    params.require(:plante).permit(:name, :price, :variety, :description)
+  end
+
 end
