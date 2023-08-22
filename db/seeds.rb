58e2e6de4b7b88f7174b8098dfc03b0357723640
Marcela Langarica Mejia <mlangaricamejia@gmail.com>
2023-08-22 15:47:55 -0400

    Group check updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,23 @@
 
 require "faker"
 
-30.times do
-  allplants = Plante.new(
-    name: Faker::Cannabis.brand,
-    price: rand(100),
-    variety: Faker::Cannabis.cannabinoid
+Plante.destroy_all
+User.destroy_all
+5.times do
+  user = User.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: Faker::Color.hex_color
   )
-  allplants.save
+  user.save!
+  rand(0...5).times do
+    plante = Plante.new(
+      name: Faker::Cannabis.brand,
+      price: rand(100),
+      variety: Faker::Cannabis.cannabinoid
+    )
+    plante.user = user
+    plante.save!
+  end
 end
