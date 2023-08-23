5c8ecffff61ac8083c10942a9b4b5c7d08c5bada
Grace Pan <grace.hrpan@gmail.com>
2023-08-23 14:23:38 -0400

    created request button with ability to add new request in indext list

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,12 +1,29 @@
 class RequestsController < ApplicationController
+  before_action :set_plante, only: %i[new create]
+
   def index
     @requests = Request.all
   end
 
-  def show
-
-  end
+  # def show
+  # end
 
   def new
+    @request = Request.new
+  end
+
+  def create
+    @request = Request.new(user: current_user, plante: @plante)
+    if @request.save
+      redirect_to requests_path
+    else
+      render 'requests', status: 422
+    end
+  end
+
+  private
+
+  def set_plante
+    @plante = Plante.find(params[:plante_id])
   end
 end
